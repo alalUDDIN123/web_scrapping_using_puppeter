e0631eb9076f2f562318133d578e03e8d9013c6a
alalUDDIN123 <alalu75666@gmail.com>
2023-07-06 22:14:56 +0530

    web_scrapping_using_puppeteer_screenshot

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+
 const puppeteer = require('puppeteer');
 
 async function takeScreenshot(url, outputPath) {
-  const browser = await puppeteer.launch({headless:"new"}); // luanch the server
-  const page = await browser.newPage(); // open a new tab
-  
-  await page.goto(url); // go to browser according to passing parameter
-  await page.screenshot({ path: outputPath }); // take screenshot
-  
-  await browser.close(); //close the borwser once taken screenshot
+  const browser = await puppeteer.launch({ headless: "new" }); // Launch a new instance of the Puppeteer browser
+  const page = await browser.newPage(); // Open a new tab in the browser
+  await page.setViewport({ width: 1080, height: 1280 }); // Set the viewport size for the page
+  await page.goto(url); // Navigate to the specified URL
+  await page.screenshot({ path: outputPath }); // Take a screenshot and save it to the specified output path
+
+  await browser.close(); // Close the browser instance once the screenshot is taken
 }
 
 // Usage example
-const webpageUrl ='https://course.masaischool.com/lectures/64915'; // example url
-const screenshotPath = 'screenshot.png'; // name of the screenshot
+const webpageUrl = 'https://sass-lang.com/guide/'; // Example URL to capture
+const screenshotPath = 'screenshot.png'; // Name of the screenshot file
 
-takeScreenshot(webpageUrl, screenshotPath) // calling function
+takeScreenshot(webpageUrl, screenshotPath) // Call the function to take a screenshot
   .then(() => console.log('Screenshot taken successfully!'))
-  .catch((error) => console.error('Error while taking screenshot:', error));
+  .catch((error) => console.error('Error while taking a screenshot:', error));
+
 
   // git push origin take_screenshot
