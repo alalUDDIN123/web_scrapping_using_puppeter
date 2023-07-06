0e4418efa7fccc3b333f049c2caef96a52e3fc52
alalUDDIN123 <alalu75666@gmail.com>
2023-07-06 21:09:18 +0530

    web_scrapping_using_puppeteer_screenshot

---
+++ index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+async function takeScreenshot(url, outputPath) {
+  const browser = await puppeteer.launch({headless:"new"}); // luanch the server
+  const page = await browser.newPage(); // open a new tab
+  
+  await page.goto(url); // go to browser according to passing parameter
+  await page.screenshot({ path: outputPath }); // take screenshot
+  
+  await browser.close(); //close the borwser once taken screenshot
+}
+
+// Usage example
+const webpageUrl ='https://course.masaischool.com/lectures/64915'; // example url
+const screenshotPath = 'screenshot.png'; // name of the screenshot
+
+takeScreenshot(webpageUrl, screenshotPath) // calling function
+  .then(() => console.log('Screenshot taken successfully!'))
+  .catch((error) => console.error('Error while taking screenshot:', error));
+
+  // git push origin take_screenshot
